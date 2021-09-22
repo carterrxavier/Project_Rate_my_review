dbea1ce8537dbc9272e0b6db7e67b61abcbe479a
Xavier Carter <80718296+carterrxavier@users.noreply.github.com>
2021-09-22 14:37:30 -0500

    more exploration. more 1 sample Ttest

--- evaluate.py
+++ evaluate.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy.stats as stats
 
 
-def get_t_test(t_var, df, var1 , var2, target, alpha, tail_type = 2, var=False):
+def get_t_test(t_var, df, var1, target, alpha=0.05, tail_type = 2, var=False , var2=None):
     '''
         This method will produce a 1 or 2 tailed t test to equate the p value to the alpha to determine whether the null hypothesis can be rejected.
         tail
@@ -13,20 +13,30 @@ def get_t_test(t_var, df, var1 , var2, target, alpha, tail_type = 2, var=False):
         if p > alpha:
             var = True
         if tail_type == 1:
-            t, p = stats.ttest_1samp(df[i],df[target], equal_var=var)
+            a = df.loc[df[target] == var1, i].to_numpy()
+            t, p = stats.ttest_1samp(a, df[target].mean())
+            print('Null Hypothesis: for {}, the {} mean when {} == {} is the same as the population mean'.format(i, target, target, var1))
+            print('Alternative hypothesis: for {}, the {} mean when {} == {} is not the same as the population mean'.format(i, target, target, var1))
+            if p/2 < alpha:
+                print('p value {} is less than alpha {} , we reject our null hypothesis'.format(p,alpha))
+            else:
+                print('p value {} is not less than alpha {} , we  fail to reject our null hypothesis'.format(p,alpha))
+            print('-----------------------------------')
+
+            
         elif tail_type == 2:
             a = df.loc[df[target] == var1, i].to_numpy()
             b = df.loc[df[target] == var2, i].to_numpy()
             t, p = stats.ttest_ind(a,b, equal_var=var)
        
-        print('Null Hypothesis: the mean value of {} is the same when {} == {} and {}'.format(i, target, var1, var2))
-        print('Alternative hypothesis: the mean value of {} is not the same when {} == {} and {}')
-        if p < alpha:
-            print('p value {} is less than alpha {} , we reject our null hypothesis'.format(p,alpha))
-        else:
-            print('p value {} is not less than alpha {} , we  fail to reject our null hypothesis'.format(p,alpha))
-        print('-----------------------------------')
-        
+            print('Null Hypothesis: the mean value of {} is the same when {} == {} and {}'.format(i, target, var1, var2))
+            print('Alternative hypothesis: the mean value of {} is not the same when {} == {} and {}')
+            if p < alpha:
+                print('p value {} is less than alpha {} , we reject our null hypothesis'.format(p,alpha))
+            else:
+                print('p value {} is not less than alpha {} , we  fail to reject our null hypothesis'.format(p,alpha))
+            print('-----------------------------------')
+
 def get_pearsons(con_var, target, alpha, df):
      for i in con_var:
         t, p = stats.pearsonr(df[i],df[target])
